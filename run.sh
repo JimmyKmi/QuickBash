efef553aad7904f355d5f52cd1da61e4621094e3
jimmy-kmi <jimmy@lzj.ac.cn>
2023-02-17 03:26:31 +0800

    管理员权限

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 设置颜色变量
-CLR_BLACK='\033[0;30m' # 黑色
+#CLR_BLACK='\033[0;30m' # 黑色
 #CLR_RED='\033[0;31m'        # 红色
 CLR_GREEN='\033[0;32m' # 绿色
 #CLR_YELLOW='\033[0;33m'     # 黄色
@@ -13,37 +13,36 @@ CLR_='\033[0m' # 重置颜色
 # DEMO
 #echo -e "${CLR_BLACK}这是黑色文本${CLR_}"
 
-# 菜单
-echo -e "${CLR_GREEN}JIMMY BASH ==${CLR_}"
-echo "============================"
-echo "请选择需要的操作："
-echo "1. 容器相关"
-echo "2. 选项2"
-echo "3. 选项3"
-echo "4. 退出"
+# 主菜单
+showMenu() {
+  echo -e "${CLR_GREEN}JIMMY BASH ==${CLR_}"
+  echo "============================"
+  PS3='Choose your favorite food: '
+  menu=("SYSTEM" "DEPLOY" "CHECK" "EXIT")
+#  foods=("SYSTEM" "DEPLOY" "CHECK" "EXIT")
+  select fav in "${menu[@]}"; do
+    case $fav in
+    "SYSTEM")
+      echo "Americans eat roughly 100 acres of $fav each day!"
+      # optionally call a function or run some code here
+      ;;
+    "DEPLOY")
+      echo "$fav is a Vietnamese soup that is commonly mispronounced like go, instead of duh."
+      # optionally call a function or run some code here
+      ;;
+    "CHECK")
+      echo "According to NationalTacoDay.com, Americans are eating 4.5 billion $fav each year."
+      # optionally call a function or run some code here
+      break
+      ;;
+    "EXIT")
+      echo "User requested exit"
+      exit
+      ;;
+    *) echo "输入的值 $REPLY 不可用" ;;
+    esac
+  done
+}
 
-# 读取用户的选择
-read -p "输入选项号码: " choice
-
-# 根据用户的选择执行相应的操作
-case $choice in
-1)
-  echo "您选择了选项1"
-  # 在这里添加选项1的操作
-  ;;
-2)
-  echo "您选择了选项2"
-  # 在这里添加选项2的操作
-  ;;
-3)
-  echo "您选择了选项3"
-  # 在这里添加选项3的操作
-  ;;
-4)
-  echo "感谢使用，再见！"
-  exit 0
-  ;;
-*)
-  echo "无效的选项，请重新选择。"
-  ;;
-esac
+# 展示主菜单
+showMenu
