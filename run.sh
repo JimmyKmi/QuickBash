92a97c82c5c967fb6c1713ebf57b5cc9300745cb
jimmy-kmi <jimmy@lzj.ac.cn>
2023-02-17 04:09:35 +0800

    管理员权限

--- run.sh
+++ run.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
 # 常用变量
-SRC_PATH='https://raw.githubusercontent.com/JimmyKmi/QuickBash/master/'
+#SRC_PATH='https://raw.githubusercontent.com/JimmyKmi/QuickBash/master/'
+
+# 执行程序
+run() {
+  curl -sSO "${SRC_PATH}/script/$1.sh" && bash "$1.sh" && rm "$1.sh" -f
+}
 
 # 主菜单
 showMenu() {
   PS3='>'
-  menu=("SYSTEM" "DEPLOY" "CHECK" "EXIT")
-  #  foods=("SYSTEM" "DEPLOY" "CHECK" "EXIT")
+  menu=("软件更新 + 基础环境配置" "关闭防火墙" "CHECK" "EXIT")
   select fav in "${menu[@]}"; do
-    echo "=============MENU=============="
     case $fav in
-    "SYSTEM")
+    "软件更新 + 基础环境配置")
       echo "Americans eat roughly 100 acres of $fav each day!"
       ;;
-    "DEPLOY")
-      echo "$fav is a Vietnamese soup that is commonly mispronounced like go, instead of duh."
+    "关闭防火墙")
+      run firewall-off
       ;;
     "CHECK")
       echo "According to NationalTacoDay.com, Americans are eating 4.5 billion $fav each year."
