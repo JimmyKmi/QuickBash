7fea4acd0a8ff63fd77bb31772e88d2192bca0b8
jimmy-kmi <jimmy@lzj.ac.cn>
2023-02-17 03:03:26 +0800

    测试颜色

--- run.sh
+++ run.sh
@@ -1,28 +1,17 @@
 #!/bin/bash
 
 # 设置颜色变量
-CLR_BLACK='\033[0;30m'
-CLR_RED='\033[0;31m'
-CLR_GREEN='\033[0;32m'
-CLR_YELLOW='\033[0;33m'
-CLR_BLUE='\033[0;34m'
-CLR_MAGENTA='\033[0;35m'
-CLR_CYAN='\033[0;36m'
-CLR_WHITE='\033[0;37m'
-CLR_='\033[0m'
-
-# 显示彩色文本
-echo -e "${CLR_BLACK}这是黑色文本${CLR_}"
-echo -e "${CLR_RED}这是红色文本${CLR_}"
-echo -e "${CLR_GREEN}这是绿色文本${CLR_}"
-echo -e "${CLR_YELLOW}这是黄色文本${CLR_}"
-echo -e "${CLR_BLUE}这是蓝色文本${CLR_}"
-echo -e "${CLR_MAGENTA}这是洋红色文本${CLR_}"
-echo -e "${CLR_CYAN}这是青色文本${CLR_}"
-echo -e "${CLR_WHITE}这是白色文本${CLR_}"
-
-# 标题
-echo "JIMMY BASH =="
+CLR_BLACK='\033[0;30m'      # 黑色
+CLR_RED='\033[0;31m'        # 红色
+CLR_GREEN='\033[0;32m'      # 绿色
+CLR_YELLOW='\033[0;33m'     # 黄色
+CLR_BLUE='\033[0;34m'       # 蓝色
+CLR_MAGENTA='\033[0;35m'    # 洋红色
+CLR_CYAN='\033[0;36m'       # 青色
+CLR_WHITE='\033[0;37m'      # 白色
+CLR_='\033[0m'              # 重置颜色
+# DEMO
+# echo -e "${CLR_BLACK}这是黑色文本${CLR_}"
 
 # 检查是否为管理员权限的用户
 if [ "$(id -u)" != "0" ]; then
@@ -33,37 +22,39 @@ fi
 sudo -i
 
 # 菜单
-#echo "请选择一个选项："
-#echo "1. 选项1"
-#echo "2. 选项2"
-#echo "3. 选项3"
-#echo "4. 退出"
-#
-## 读取用户的选择
-#read -p "输入选项号码: " choice
-#
-## 根据用户的选择执行相应的操作
-#case $choice in
-#    1)
-#        echo "您选择了选项1"
-#        # 在这里添加选项1的操作
-#        ;;
-#    2)
-#        echo "您选择了选项2"
-#        # 在这里添加选项2的操作
-#        ;;
-#    3)
-#        echo "您选择了选项3"
-#        # 在这里添加选项3的操作
-#        ;;
-#    4)
-#        echo "感谢使用，再见！"
-#        exit 0
-#        ;;
-#    *)
-#        echo "无效的选项，请重新选择。"
-#        ;;
-#esac
+echo "${CLR_GREEN}JIMMY BASH ==${CLR_}"
+echo "============================"
+echo "请选择需要的操作："
+echo "1. 容器相关"
+echo "2. 选项2"
+echo "3. 选项3"
+echo "4. 退出"
+
+# 读取用户的选择
+read -p "输入选项号码: " choice
+
+# 根据用户的选择执行相应的操作
+case $choice in
+    1)
+        echo "您选择了选项1"
+        # 在这里添加选项1的操作
+        ;;
+    2)
+        echo "您选择了选项2"
+        # 在这里添加选项2的操作
+        ;;
+    3)
+        echo "您选择了选项3"
+        # 在这里添加选项3的操作
+        ;;
+    4)
+        echo "感谢使用，再见！"
+        exit 0
+        ;;
+    *)
+        echo "无效的选项，请重新选择。"
+        ;;
+esac
 
 # 退出管理员权限
 exit
