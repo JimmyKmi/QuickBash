867a2219b65db2ade040c94069195b15ec17b7c7
jimmy-kmi <jimmy@lzj.ac.cn>
2023-02-17 04:00:56 +0800

    管理员权限

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # QuickBash 懒虫专用回车
 
->该项目仍在开发阶段，使用前请仔细阅读脚本
+> 该项目仍在开发阶段，使用前请仔细阅读脚本
+
 ## 起源
 
 > 一切源于懒
@@ -20,9 +21,16 @@
 
 | \            | Alibaba 3      | Alibaba 2 | Anolis 8 | CentOS | Ubuntu |
 |--------------|----------------|-----------|----------|--------|--------|
+| SYSTEM       |
+| firewall-off | 3.2104 LTS x64 | --        | --       | --     | --     |
+|              |
+| DEPLOY       |
 | docker       | 3.2104 LTS x64 | --        | --       | --     | --     |
 | portainer    | 3.2104 LTS x64 | --        | --       | --     | --     |
+|              |
+| FAST-DEPLOY  |
 | firewall-off | 3.2104 LTS x64 | --        | --       | --     | --     |
 
 ## 如何使用
+
 > curl -sSO https://raw.githubusercontent.com/JimmyKmi/QuickBash/master/run.sh && bash run.sh && rm run.sh -f
--- run.sh
+++ run.sh
@@ -1,45 +1,31 @@
 #!/bin/bash
 
-# 设置颜色变量
-#CLR_BLACK='\033[0;30m' # 黑色
-#CLR_RED='\033[0;31m'        # 红色
-CLR_GREEN='\033[0;32m' # 绿色
-#CLR_YELLOW='\033[0;33m'     # 黄色
-#CLR_BLUE='\033[0;34m'       # 蓝色
-#CLR_MAGENTA='\033[0;35m'    # 洋红色
-#CLR_CYAN='\033[0;36m'       # 青色
-#CLR_WHITE='\033[0;37m'      # 白色
-CLR_='\033[0m' # 重置颜色
-# DEMO
-#echo -e "${CLR_BLACK}这是黑色文本${CLR_}"
+# 常用变量
+SRC_PATH='https://raw.githubusercontent.com/JimmyKmi/QuickBash/master/'
 
 # 主菜单
 showMenu() {
-  echo -e "${CLR_GREEN}JIMMY BASH ==${CLR_}"
-  echo "============================"
-  PS3='Choose your favorite food: '
+  echo "=============MENU=============="
+  PS3='>'
   menu=("SYSTEM" "DEPLOY" "CHECK" "EXIT")
 #  foods=("SYSTEM" "DEPLOY" "CHECK" "EXIT")
   select fav in "${menu[@]}"; do
     case $fav in
     "SYSTEM")
       echo "Americans eat roughly 100 acres of $fav each day!"
-      # optionally call a function or run some code here
       ;;
     "DEPLOY")
       echo "$fav is a Vietnamese soup that is commonly mispronounced like go, instead of duh."
-      # optionally call a function or run some code here
       ;;
     "CHECK")
       echo "According to NationalTacoDay.com, Americans are eating 4.5 billion $fav each year."
-      # optionally call a function or run some code here
       break
       ;;
     "EXIT")
       echo "User requested exit"
       exit
       ;;
-    *) echo "输入的值 $REPLY 不可用" ;;
+    *) echo "VALUE [$REPLY] UNAVAILABLE" ;;
     esac
   done
 }
