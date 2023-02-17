7cbc4e53ff1c23157a64f1a49d373efdff78fb8f
jimmy-kmi <jimmy@lzj.ac.cn>
2023-02-17 17:28:34 +0800

    完善PORTAINER提示

--- script/portainer-ce-install.sh
+++ script/portainer-ce-install.sh
@@ -3,5 +3,11 @@
 # 创建 Docker volume
 docker volume create portainer_data
 
-# 启动 Portainer Community Edition
+# 启动 Portainer Community Edition，9443 为对外端口
 docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+
+# 获取服务器的公网 IP 地址
+IPADDRESS=$(curl https://api.ipify.org)
+
+# 输出安装成功提示信息
+echo "PORTAINER 安装成功，请访问 https://$IPADDRESS:9443/ 进行初始化配置。如果长时间未配置，则需要重启容器才能进入配置页面。如果 IP 地址不正确，请手动输入正确的 IP 地址访问。"
