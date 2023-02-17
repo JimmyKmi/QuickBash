a69f962e9504dd7f06cf6fbd53c641d2345c6689
jimmy-kmi <jimmy@lzj.ac.cn>
2023-02-17 17:32:32 +0800

    添加对 Ubuntu 适配

--- script/docker-ce-install.sh
+++ script/docker-ce-install.sh
@@ -7,17 +7,22 @@ if command -v dnf &>/dev/null; then
 elif command -v yum &>/dev/null; then
   PACKAGE_HEAD="yum-config-manager"
   PACKAGE_HEAD_SHORT="yum"
+elif command -v apt-get &>/dev/null; then
+  PACKAGE_HEAD="add-apt-repository"
+  PACKAGE_HEAD_SHORT="apt-get"
 else
   echo "系统不在兼容列表"
   exit 1
 fi
 
 # 添加 docker-ce 的包管理器源
+echo "正在添加 docker-ce 的包管理器源..."
 ${PACKAGE_HEAD} --add-repo=https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
 
 # 如果是 Alibaba Cloud Linux 3，安装 dnf-releasever-adapter 插件
 if [ -f /etc/alinux-release ]; then
   if grep -q 'Alibaba Cloud Linux release 3' /etc/alinux-release; then
+    echo "检测到 Alibaba Cloud Linux，正在安装 dnf-releasever-adapter 插件..."
     if command -v dnf &>/dev/null; then
       dnf -y install dnf-plugin-releasever-adapter --repo alinux3-plus
     elif command -v yum &>/dev/null; then
@@ -27,28 +32,35 @@ if [ -f /etc/alinux-release ]; then
 fi
 
 # 安装 Docker
+echo "正在安装 Docker..."
 ${PACKAGE_HEAD_SHORT} -y install docker-ce --nobest
 
 # 启动 Docker
+echo "正在启动 Docker..."
 systemctl start docker
 
 # 设置 Docker 开机自启动
+echo "正在设置 Docker 开机自启动..."
 systemctl enable docker
 
 # 修改 Docker 配置文件，启用 Docker Hub 国内镜像
-cat << EOF > /etc/docker/daemon.json
+cat <<EOF >/etc/docker/daemon.json
 {
-    "registry-mirrors": [
-        "http://hub-mirror.c.163.com",
-        "https://docker.mirrors.ustc.edu.cn",
-        "https://registry.docker-cn.com"
-    ]
+  "registry-mirrors": [
+    "http://hub-mirror.c.163.com",
+    "https://docker.mirrors.ustc.edu.cn",
+    "https://registry.docker-cn.com",
+    "https://dockerhub.azk8s.cn",
+    "https://hub-mirror.c.163.com",
+    "https://mirror.baidubce.com"
+  ]
 }
 EOF
 
 # 重启 Docker
+echo "正在重启 Docker..."
 systemctl restart docker
 
 # 检查 Docker 是否安装成功
+echo "正在检查 Docker 是否安装成功..."
 docker info
-
