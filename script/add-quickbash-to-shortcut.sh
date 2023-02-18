7ec9a01a15969064a99148ce9f9d3947d9b81f0e
jimmy-kmi <jimmy@lzj.ac.cn>
2023-02-18 23:07:53 +0800

    添加快捷指令 修复

--- script/add-quickbash-to-shortcut.sh
+++ script/add-quickbash-to-shortcut.sh
@@ -9,6 +9,29 @@ if [ "$confirm" != "y" ]; then
   exit
 fi
 
+echo "正在检测网络..."
+ping_google_result=$(ping -c 3 -W 1 "www.google.com")                        # 发送3个ICMP报文
+avg_google_rtt=$(echo "$ping_google_result" | awk -F/ '/^rtt/ { print $5 }') # 解析输出，提取平均延迟
+
+if [ -z "$avg_google_rtt" ]; then
+  echo "海外连接失败..."
+  # 检测内地网络
+  ping_baidu_result=$(ping -c 3 -W 1 "www.baidu.com")                        # 发送3个ICMP报文
+  avg_baidu_rtt=$(echo "$ping_baidu_result" | awk -F/ '/^rtt/ { print $5 }') # 解析输出，提取平均延迟
+
+  if [ -z "$avg_baidu_rtt" ]; then
+    echo "内地连接失败..."
+    echo "请检查网络状况."
+    exit
+  else
+    echo "内地延迟: $avg_baidu_rtt ms，使用 Gitee 源."
+    GIT_ADDR="https://gitee.com/jimmykmi/QuickBash/raw/master/script/"
+  fi
+else
+  echo "海外延迟: $avg_google_rtt ms，使用 GitHub 源."
+  GIT_ADDR="https://raw.githubusercontent.com/JimmyKmi/QuickBash/master/script/"
+fi
+
 # 判断 GIT_ADDR
 if [ "$GIT_ADDR" = "https://gitee.com/jimmykmi/QuickBash/raw/master/script/" ]; then
   sudo curl -sSO https://gitee.com/jimmykmi/QuickBash/raw/master/run.sh && bash run.sh && rm run.sh -f
