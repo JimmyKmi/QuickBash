cbb52361b399634923c371e5ef01e4a503a835b8
jimmy-kmi <jimmy@lzj.ac.cn>
2023-02-18 17:05:54 +0800

    添加 Ubuntu 国内镜像兼容

--- script/docker-ce-install.sh
+++ script/docker-ce-install.sh
@@ -34,7 +34,6 @@ if [ -z "$PACKAGE_TAG" ]; then
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
   # 添加 Docker 镜像源
   echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-
 else
   # yum / dnf 包管理器
   ${PACKAGE_HEAD} --add-repo=https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
