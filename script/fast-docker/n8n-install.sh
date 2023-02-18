1ef7d67d2635a355be83ee248e9e58c5dde48345
jimmy-kmi <jimmy@lzj.ac.cn>
2023-02-19 03:50:57 +0800

    BUG

--- script/fast-docker/n8n-install.sh
+++ script/fast-docker/n8n-install.sh
@@ -4,7 +4,23 @@
 # 停止并删除旧的 myn8n 容器
 docker stop myn8n
 docker rm myn8n
-docker volume rm n8n_data
+
+# 设置 n8n 的数据卷名称
+VOLUME_NAME="n8n_data"
+
+# 判断数据卷是否存在
+if docker volume ls | grep "$VOLUME_NAME"; then
+  # 数据卷存在，询问是否保留数据卷
+  read -t 10 -p "是否保留数据卷 $VOLUME_NAME？[Y/n] " KEEP_VOLUME
+  if [[ "$KEEP_VOLUME" == "n" || "$KEEP_VOLUME" == "N" ]]; then
+    # 不保留数据卷，删除数据卷
+    docker volume rm "$VOLUME_NAME"
+    echo "已删除数据卷 $VOLUME_NAME。"
+  else
+    # 保留数据卷，显示提示信息
+    echo "已保留数据卷 $VOLUME_NAME。"
+  fi
+fi
 
 # 安装 n8n
 docker run -d \
@@ -12,7 +28,8 @@ docker run -d \
   --name myn8n \
   -v n8n_data:/home/node/.n8n \
   -e NODE_ENV=production \
-  n8nio/n8n
+  n8nio/n8n \
+  n8n start --tunnel
 
 # 输出安装成功信息
 echo "n8n 安装成功！"
--- script/fast-docker/nodered-install.sh
+++ script/fast-docker/nodered-install.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 # 快速安装 NODE-RED
 
+echo "注意: nodered 脚本还处于试验阶段，可能存在未知的问题。"
+echo "请在确认知晓后继续进行安装。"
+while true; do
+  read -t 10 -p "您是否确认继续安装？ [Y/n] " yn
+  if [[ "$yn" == "y" || "$yn" == "Y" || "$yn" == "" ]]; then
+    break
+  elif [[ "$yn" == "n" || "$yn" == "N" ]]; then
+    echo "您已取消安装。"
+    exit 0
+  else
+    echo "无效的输入，请重新输入。"
+  fi
+done
+
 echo "请为 Node-RED 设置用户名与密码。"
 
 # 提示用户输入用户名和密码
@@ -23,6 +37,25 @@ while true; do
     echo "用户名为空或包含空格，请重新输入。"
   fi
 done
+
+# 设置 Node-RED 的数据卷名称
+VOLUME_NAME="node_red_data"
+
+# 判断数据卷是否存在
+if docker volume ls | grep "$VOLUME_NAME"; then
+  # 数据卷存在，询问是否保留数据卷
+  read -t 10 -p "是否保留数据卷 $VOLUME_NAME？[Y/n] " KEEP_VOLUME
+  if [[ "$KEEP_VOLUME" == "n" || "$KEEP_VOLUME" == "N" ]]; then
+    # 不保留数据卷，删除数据卷
+    docker volume rm "$VOLUME_NAME"
+    echo "已删除数据卷 $VOLUME_NAME。"
+  else
+    # 保留数据卷，显示提示信息
+    echo "已保留数据卷 $VOLUME_NAME。"
+    echo "请确保设置的密码与原数据卷密码保持一致。"
+    sleep 5s
+  fi
+fi
 
 # 安装 Node-RED
 docker run -d \
