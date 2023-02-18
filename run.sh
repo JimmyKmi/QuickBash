8bca1fcc8109b447baf3de11cf682cdc0099c285
jimmy-kmi <jimmy@lzj.ac.cn>
2023-02-18 16:26:41 +0800

    加入Gitee源

--- run.sh
+++ run.sh
@@ -2,15 +2,39 @@
 
 # 执行程序
 run() {
+  # 检测网络
+  echo "正在检测网络..."
+  ping_google_result=$(ping -c 3 "www.google.com")                             # 发送3个ICMP报文
+  avg_google_rtt=$(echo "$ping_google_result" | awk -F/ '/^rtt/ { print $5 }') # 解析输出，提取平均延迟
+
+  if [ -z "$avg_google_rtt" ]; then
+    echo "海外连接失败..."
+    # 检测内地网络
+    ping_baidu_result=$(ping -c 3 "www.baidu.com")                             # 发送3个ICMP报文
+    avg_baidu_rtt=$(echo "$ping_baidu_result" | awk -F/ '/^rtt/ { print $5 }') # 解析输出，提取平均延迟
+
+    if [ -z "$avg_google_rtt" ]; then
+      echo "内地连接失败..."
+      echo "请检查网络状况."
+      return 1
+    else
+      echo "内地延迟: $avg_baidu_rtt ms，使用 Gitee 源."
+      GIT_ADDR="https://gitee.com/jimmykmi/QuickBash/raw/master/script/"
+    fi
+  else
+    echo "海外延迟: $avg_google_rtt ms，使用 GitHub 源."
+    GIT_ADDR="https://raw.githubusercontent.com/JimmyKmi/QuickBash/master/script/"
+  fi
+
   # 从指定URL下载脚本，执行脚本，并删除脚本文件
-  curl -sSO https://raw.githubusercontent.com/JimmyKmi/QuickBash/master/script/$1.sh && bash $1.sh && rm $1.sh -f
+  curl -sSO $GIT_ADDR$1.sh && bash $1.sh && rm $1.sh -f
 }
 
 # 主菜单
 menuMain() {
-  PS3='[]>' # 设置菜单提示符
+  PS3='[]>'                                # 设置菜单提示符
   menu=("更新环境、安装必备程序" "关闭防火墙" "安装程序" "退出") # 定义菜单项
-  select fav in "${menu[@]}"; do # 显示菜单并等待用户输入
+  select fav in "${menu[@]}"; do           # 显示菜单并等待用户输入
     case $fav in
     "更新环境、安装必备程序")
       run env
