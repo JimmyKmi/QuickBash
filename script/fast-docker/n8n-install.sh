736d6e633fdd64c8bf77147fa08e5b721c476eca
jimmy-kmi <jimmy@lzj.ac.cn>
2023-02-19 01:17:50 +0800

    简化 n8n 密码流程

--- script/fast-docker/n8n-install.sh
+++ script/fast-docker/n8n-install.sh
@@ -6,37 +6,11 @@ docker stop myn8n
 docker rm myn8n
 docker volume rm n8n_data
 
-echo "请为 n8n 设置用户名与密码。"
-
-# 提示用户输入用户名和密码
-while true; do
-  # shellcheck disable=SC2162
-  read -p "请输入要创建的用户名: " USERNAME
-  if [[ -n "$USERNAME" && ! "$USERNAME" =~ " " ]]; then
-    # shellcheck disable=SC2162
-    read -p "请输入要创建的密码: " -s PASSWORD
-    echo
-    # shellcheck disable=SC2162
-    read -p "请再次输入密码以确认: " -s PASSWORD2
-    echo
-    if [[ "$PASSWORD" = "$PASSWORD2" && -n "$PASSWORD" && ! "$PASSWORD" =~ " " ]]; then
-      break
-    else
-      echo "两次输入的密码不一致或密码为空或包含空格，请重新输入。"
-    fi
-  else
-    echo "用户名为空或包含空格，请重新输入。"
-  fi
-done
-
 # 安装 n8n
 docker run -d \
   -p 25678:5678 \
   --name myn8n \
   -v n8n_data:/home/node/.n8n \
-  -e N8N_BASIC_AUTH_ACTIVE=true \
-  -e N8N_BASIC_AUTH_USER="$USERNAME" \
-  -e N8N_BASIC_AUTH_PASSWORD="$PASSWORD" \
   -e NODE_ENV=production \
   n8nio/n8n
 
