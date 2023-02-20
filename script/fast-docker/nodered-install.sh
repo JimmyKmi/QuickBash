e3fd4cb37b10dfa28bb3aa8a227decec67a94b19
{jimmy@lzj.ac.cn} <jimmy_kmi@outlook.fr>
2023-02-20 10:37:27 +0800

    更新 node-red

--- script/fast-docker/nodered-install.sh
+++ script/fast-docker/nodered-install.sh
@@ -15,28 +15,9 @@ while true; do
   fi
 done
 
-echo "请为 Node-RED 设置用户名与密码。"
-
-# 提示用户输入用户名和密码
-while true; do
-  # shellcheck disable=SC2162
-  read -p "请输入要创建的用户名: " USERNAME
-  if [[ -n "$USERNAME" && ! "$USERNAME" =~ " " ]]; then
-    # shellcheck disable=SC2162
-    read -p "请输入要创建的密码: " -s PASSWORD
-    echo
-    # shellcheck disable=SC2162
-    read -p "请再次输入密码以确认: " -s PASSWORD2
-    echo
-    if [[ "$PASSWORD" = "$PASSWORD2" && -n "$PASSWORD" && ! "$PASSWORD" =~ " " ]]; then
-      break
-    else
-      echo "两次输入的密码不一致或密码为空或包含空格，请重新输入。"
-    fi
-  else
-    echo "用户名为空或包含空格，请重新输入。"
-  fi
-done
+# 停止并删除旧的 mynodered 容器
+docker stop mynodered
+docker rm mynodered
 
 # 设置 Node-RED 的数据卷名称
 VOLUME_NAME="node_red_data"
@@ -52,25 +33,37 @@ if docker volume ls | grep "$VOLUME_NAME"; then
   else
     # 保留数据卷，显示提示信息
     echo "已保留数据卷 $VOLUME_NAME。"
-    echo "请确保设置的密码与原数据卷密码保持一致。"
-    sleep 5s
   fi
 fi
 
+# 请为 Node-RED 设置用户名与密码。
+echo "请为 Node-RED 设置用户名与密码。"
+
+# 提示用户输入用户名和密码
+while true; do
+  read -p "请输入要创建的用户名: " USERNAME
+  read -p "请输入要创建的密码: " -s PASSWORD
+  echo
+  read -p "请再次输入密码以确认: " -s PASSWORD2
+  echo
+  if [ "$PASSWORD" = "$PASSWORD2" ]; then
+    break
+  else
+    echo "两次输入的密码不一致，请重新输入。"
+  fi
+done
+
 # 安装 Node-RED
-# shellcheck disable=SC2086
-docker run -d -it \
-  -p 21800:1880 \
+docker run -d -p 21800:1880 \
   --name mynodered \
   -v node_red_data:/data \
-  -e TZ="Asia/Shanghai" \
-  -e NODE_RED_USERNAME=$USERNAME \
-  -e NODE_RED_PASSWORD=$PASSWORD \
+  -e NODE_RED_ENABLE_PROJECTS=true \
+  -e NODE_RED_USERNAME="$USERNAME" \
+  -e NODE_RED_PASSWORD="$PASSWORD" \
   nodered/node-red
-#  -e NODE_RED_ENABLE_PROJECTS=true \
 
 # 输出安装成功信息
 echo "Node-RED 安装成功！"
+echo "请通过 http://<your_server_ip>:21800 访问 Node-RED。"
 echo "用户名: $USERNAME"
 echo "密码: $PASSWORD"
-echo "请通过 http://<your_server_ip>:21800 访问 Node-RED。"
